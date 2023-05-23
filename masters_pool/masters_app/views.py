d157d3f6c5aaab72e45788c81a0ff42a9f761b6d
Luke Martin <lmartin9@uiowa.edu>
2023-05-22 21:29:04 -0500

    created signin html page

--- masters_pool/masters_app/views.py
+++ masters_pool/masters_app/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello I am working")
+    return render(request, "index.html")
+
+
+def signup(request):
+    return render(request, "signup.html")
+
+
+def signin(request):
+    return render(request, "signin.html")
+
+
+def signout(request):
+    pass
--- masters_pool/masters_pool/settings.py
+++ masters_pool/masters_pool/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'masters_pool.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
